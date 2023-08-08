bc32acff6a0c6bfc188b7cbbe92b8d2ac9b4f561
Alex Ehrnschwender <alexanderscott46@gmail.com>
2023-08-08 13:38:13 -0700

    fix db scripts

--- scripts/migrate_db.py
+++ scripts/migrate_db.py
@@ -4,6 +4,6 @@ DB_NAME = "burrito_points"
 DB_ENDPOINT_URL = "http://localhost:8000"
 
 if __name__ == "__main__":
-    db = BotDB(DB_NAME, DB_ENDPOINT_URL)
+    db = BotDB(DB_NAME, **{'endpoint_url': DB_ENDPOINT_URL})
     db.create_table()
     print(f"{DB_NAME} created successfully!")
--- scripts/truncate_db.py
+++ scripts/truncate_db.py
@@ -5,6 +5,6 @@ DB_ENDPOINT_URL = "http://localhost:8000"
 
 if __name__ == "__main__":
     #db = BurritoDB(DB_NAME)
-    db = BotDB(DB_NAME, DB_ENDPOINT_URL)
+    db = BotDB(DB_NAME, **{'endpoint_url': DB_ENDPOINT_URL})
     db.truncate_table()
     print(f"{DB_NAME} truncated successfully!")
